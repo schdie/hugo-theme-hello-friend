e8f6ae00db4319689f83b3c8a49fc0a2365a111f
Diego Scheuermann <4019285+schdie@users.noreply.github.com>
2023-11-02 14:00:24 -0300

    needs to refactor but would do for now

--- assets/js/frases.js
+++ assets/js/frases.js
@@ -1,15 +1,23 @@
 document.addEventListener("DOMContentLoaded", function(){
 // importing quotes
-	fetch("frases.txt")
+	fetch("quotes_es.txt")
 		.then((res) => res.text())
 		.then((text) => {
 			// create an array from the text file separating new lines
 			var textArray = text.split('\n');
-			// get a random item from the array
+			// not the pretiest
 			let textArrayLength = textArray.length - 1
+			// get a random item from the array
 			var quote = textArray[Math.floor(Math.random()*textArrayLength)];
+			// let's add an ID for better control
+			document.getElementsByClassName('framed')[0].id="quoteofday";
 			// replace the quote
 			document.getElementsByClassName('framed')[0].innerHTML = quote;
+			// fancy
+			document.getElementById('quoteofday').setAttribute("style", "opacity: 1");
+			console.log(textArray);
 		})
 	.catch((e) => console.error(e));
+
+
 });
