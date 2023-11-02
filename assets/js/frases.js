baf1ce5b41a85748d79251fb908ed3866cd8234e
Diego Scheuermann <4019285+schdie@users.noreply.github.com>
2023-11-01 22:29:34 -0300

    quote of day fixes

--- assets/js/frases.js
+++ assets/js/frases.js
@@ -3,10 +3,11 @@ document.addEventListener("DOMContentLoaded", function(){
   fetch("frases.txt")
     .then((res) => res.text())
 	.then((text) => {
-	  // create an array from the text file separating on new lines
+	  // create an array from the text file separating new lines
       var textArray = text.split('\n');
       // get a random item from the array
-	  var quote = textArray[Math.floor(Math.random()*textArray.length)];
+      let textArrayLength = textArray.length - 1
+	  var quote = textArray[Math.floor(Math.random()*textArrayLength)];
 	  // replace the quote
       document.getElementsByClassName('framed')[0].innerHTML = quote;
     })
