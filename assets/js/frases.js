20fbe97c816a265cdcdba3b2e39a5d48a953a91c
Diego Scheuermann <4019285+schdie@users.noreply.github.com>
2023-11-01 22:34:10 -0300

    identation fixes

--- assets/js/frases.js
+++ assets/js/frases.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
-  // importing quotes
-  fetch("frases.txt")
-    .then((res) => res.text())
-	.then((text) => {
-	  // create an array from the text file separating new lines
-      var textArray = text.split('\n');
+// importing quotes
+	fetch("frases.txt")
+		.then((res) => res.text())
+		.then((text) => {
+			// create an array from the text file separating new lines
+			var textArray = text.split('\n');
       // get a random item from the array
-      let textArrayLength = textArray.length - 1
-	  var quote = textArray[Math.floor(Math.random()*textArrayLength)];
-	  // replace the quote
-      document.getElementsByClassName('framed')[0].innerHTML = quote;
-    })
-  .catch((e) => console.error(e));
+			let textArrayLength = textArray.length - 1
+			var quote = textArray[Math.floor(Math.random()*textArrayLength)];
+			// replace the quote
+			document.getElementsByClassName('framed')[0].innerHTML = quote;
+		})
+	.catch((e) => console.error(e));
 });
