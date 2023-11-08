e3b59a10c23b512befe9e2697e66626cf7072bf7
Diego Scheuermann <4019285+schdie@users.noreply.github.com>
2023-11-08 12:21:15 -0300

    un-reverted refactor 2

--- assets/js/frases.js
+++ assets/js/frases.js
@@ -1,36 +1,39 @@
-// retrieve a line of text from a file
-function quoteofday() {
+// importing the quotes from file
+function quotesfromfile() {
 // importing file with quotes
 	return fetch("quotes_es.txt")
 		.then((res) => res.text())
 		.then((text) => {
 			// create an array from the text file separating on new lines
 			var textArray = text.split('\n');
-			// not the pretiest
-			let textArrayLength = textArray.length - 1
-			// get a random item from the array
-			var quote = textArray[Math.floor(Math.random()*textArrayLength)];
-			return quote;
-			// let's add an ID for better control
-			//document.getElementsByClassName('framed')[0].id="quoteofday";
+			return textArray;
 		})
 	.catch((e) => console.error(e));
 }
 
-// add the first quote
-quoteofday().then(quote => {
+// getting a random quote
+function randomquote() { quotesfromfile().then(textArray => {
+	// the array with the quotes
+	const quotes = textArray;
+	// not proud
+	const quoteslength = textArray.length - 1;
+	// get a random quote from the array
+	var quote = quotes[Math.floor(Math.random()*quoteslength)];
+	// insert it into the page
 	document.getElementsByClassName('framed')[0].innerHTML = quote;
-});
+	//return quote;
+	});
+}
 
 // after most of the content was loaded
 document.addEventListener("DOMContentLoaded", function(){
-	// make the framed div visible
+	// quote on load
+	randomquote();
+	// show the div for the first time
 	document.getElementsByClassName('framed')[0].setAttribute("style", "opacity: 1");
 	// monitor the div for clicks
 	document.getElementsByClassName('framed')[0].addEventListener('click', function() {
-		quoteofday().then(quote => {
-			// change the quote on every click
-			document.getElementsByClassName('framed')[0].innerHTML = quote;
-		});
+		// change the quote
+		randomquote();
 	});
 });
