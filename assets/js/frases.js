fd5742902424fe2e538c86dd7ed37a42a8de50f5
Diego Scheuermann <4019285+schdie@users.noreply.github.com>
2023-12-20 22:48:32 -0300

    changed logic in frases.js

--- assets/js/frases.js
+++ assets/js/frases.js
@@ -10,29 +10,51 @@ function quotesfromfile() {
 		});
 }
 
-// getting a random quote
-function randomquote() { quotesfromfile().then(textArray => {
-	// the array with the quotes
-	const quotes = textArray;
-	//console.log("textArray: ", textArray);
-	// declare quote
-	let quote;
+// randomize quotes in the array
+function randomizequotes() { return quotesfromfile().then(textArray => {
+	// remove the last empty item
+	let tArrayRand = textArray.slice(0, -1)
+	// randomization
+	for (let i = tArrayRand.length - 1; i > 0; i--) {
+		const rand = Math.floor(Math.random() * (i + 1));
+		[tArrayRand[i], tArrayRand[rand]] = [tArrayRand[rand], tArrayRand[i]];
+  }
+	// return the quotes randomized 
+	return tArrayRand;
+	});
+}
 
-	function loopQuote() {
-		// get a random quote from the array
-		quote = quotes[Math.floor(Math.random()*(quotes.length - 1))];
-		// insert it into the page
-		document.getElementsByClassName('framed')[0].innerHTML = quote;
-		document.getElementsByClassName('framed')[0].setAttribute("style", "opacity: 1");
-		// the css animation is 1 sec, ~3900 is probably safer
-		setTimeout(function(){
-			document.getElementsByClassName('framed')[0].setAttribute("style", "opacity: 0");
-		},3750);
-   // and schedule a repeat
-   setTimeout(loopQuote, 5000);
-	}
-	// automagically loop through the quotes
-	loopQuote();
+// lets cycle through each quote on the framed div
+function showquotes() {
+	// recover the randomized quotes array
+	const rQuotesArray = randomizequotes();
+	// rQAresolved is the resolved promise
+	rQuotesArray.then(function (rQAresolved){
+		// set a counter
+		let qCounter = 0;
+		// logic
+		function delayedQuotes() {
+			// get the current quote and move the counter
+			document.getElementsByClassName('framed')[0].innerHTML = rQAresolved[qCounter];
+			document.getElementsByClassName('framed')[0].setAttribute("style", "opacity: 1");
+			qCounter++;
+
+			// pause for 4000 ms, quote is shown for 2900ms because the CSS animation is 1000ms, there's 100ms of "buffer"
+			if (qCounter < (rQAresolved.length)) {
+				// inception
+				setTimeout(function(){
+					document.getElementsByClassName('framed')[0].setAttribute("style", "opacity: 0");
+				},2900);
+				// restart logic
+				setTimeout(delayedQuotes, 4000);
+			} else {
+				qCounter = 0;
+				// restart logic from the zero
+				setTimeout(delayedQuotes, 4000);
+			}
+		}
+		// first call
+		delayedQuotes(); 
 	});
 }
 
@@ -40,6 +62,6 @@ function randomquote() { quotesfromfile().then(textArray => {
 document.addEventListener("DOMContentLoaded", function(){
 	// only ask for quotes on the main page
 	if (location.pathname == "/") {
-		randomquote();
+		showquotes();
 	}
 });
