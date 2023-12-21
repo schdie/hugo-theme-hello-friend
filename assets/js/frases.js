825c19ab88039483517f4ed8931a4a979aad6ef9
Diego Scheuermann <4019285+schdie@users.noreply.github.com>
2023-12-21 12:43:35 -0300

    first quote should always be the same

--- assets/js/frases.js
+++ assets/js/frases.js
@@ -12,13 +12,19 @@ function quotesfromfile() {
 
 // randomize quotes in the array
 function randomizequotes() { return quotesfromfile().then(textArray => {
+	// save the first item
+	let fQuote = textArray[0];
+	// temporarily remove the first item
+	textArray.shift();
 	// remove the last empty item
-	let tArrayRand = textArray.slice(0, -1)
+	let tArrayRand = textArray.slice(0, -1);
 	// randomization
 	for (let i = tArrayRand.length - 1; i > 0; i--) {
 		const rand = Math.floor(Math.random() * (i + 1));
 		[tArrayRand[i], tArrayRand[rand]] = [tArrayRand[rand], tArrayRand[i]];
   }
+  // re-insert the first item at the beggining of the array
+  tArrayRand.unshift(fQuote);
 	// return the quotes randomized 
 	return tArrayRand;
 	});
@@ -30,8 +36,6 @@ function showquotes() {
 	const rQuotesArray = randomizequotes();
 	// rQAresolved is the resolved promise
 	rQuotesArray.then(function (rQAresolved){
-		// debug
-		console.log("rQAresolved: ", rQAresolved);
 		// set a counter
 		let qCounter = 0;
 		// logic
@@ -43,12 +47,21 @@ function showquotes() {
 
 			// pause for 4000 ms, quote is shown for 2900ms because the CSS animation is 1000ms, there's 100ms of "buffer"
 			if (qCounter < (rQAresolved.length)) {
-				// a delay inside a delay for the CSS animation
-				setTimeout(function(){
-					document.getElementsByClassName('framed')[0].setAttribute("style", "opacity: 0");
-				},2900);
-				// restart logic
-				setTimeout(delayedQuotes, 4000);
+				if (qCounter !== 1) {
+					// a delay inside a delay for the CSS animation
+					setTimeout(function(){
+						document.getElementsByClassName('framed')[0].setAttribute("style", "opacity: 0");
+					},2900);
+					// restart logic				
+					setTimeout(delayedQuotes, 4000);
+				} else { // only for the first quote
+					// a delay inside a delay for the CSS animation
+					setTimeout(function(){
+						document.getElementsByClassName('framed')[0].setAttribute("style", "opacity: 0");
+					},6700);
+					// restart logic
+					setTimeout(delayedQuotes, 8000);
+				}
 			} else {
 				// set the counter to zero for the cycle should repeat indefinitely
 				qCounter = 0;
@@ -57,7 +70,7 @@ function showquotes() {
 					document.getElementsByClassName('framed')[0].setAttribute("style", "opacity: 0");
 				},2900);
 				// restart logic from the zero
-				setTimeout(delayedQuotes, 4000);
+				setTimeout(delayedQuotes, 4000);	
 			}
 		}
 		// first call
